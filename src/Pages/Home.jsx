b7c95d1118cd40e848df9de1509bb561c7595a4b
Octavyan Putra Ramadhan <octavyan4@gmail.com>
2023-10-22 16:59:27 +0700

    first commit

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,56 @@
+import BoxClassIg from "../components/BoxClassIg"
+import BoxTextAnonim from "../components/BoxTextAnonim"
+import Navbar from "../components/Navbar"
+
+const Home = () => {
+	return (
+		<div className="text-white px-[10%]" id="Home">
+			<Navbar />
+			<div className="lg:hidden">
+				<div className="font-bold text-5xl text-center py-20 ">XII PPLG 2</div>
+				<div
+					className="border-2 flex justify-between px-10 text-4xl font-bold py-2 relative"
+					id="TotalSiswa">
+					<div id="AngkaGradientBlue" className="flex items-center justify-center ">
+						03
+					</div>
+					<div className="">
+						<span id="AngkaGradientBlue" className="text-5xl">
+							0
+						</span>
+						<span id="AngkaGradientPink" className="text-5xl">
+							7
+						</span>
+					</div>
+					<div id="AngkaGradientPink" className="flex items-center justify-center">
+						21
+					</div>
+				</div>
+
+				<div className="grid grid-cols-2 md:grid-cols-2 gap-7 md:gap-5">
+					<div className="grid gap-2 md:gap-0 md:grid-cols-1">
+						<BoxClassIg />
+					</div>
+					<div className="grid gap-2 md:gap-0 md:grid-cols-1">
+						<BoxTextAnonim />
+					</div>
+				</div>
+			</div>
+
+			{/* Dekstop */}
+			<div className="hidden lg:block">
+				<div className="flex justify-center items-center flex-col h-[100vh]">
+					<h5 className="text-[1.4rem] font-semibold">Hi, Visitor!</h5>
+					<h1 className="text-7xl font-extrabold" id="Glow">
+						WELCOME
+					</h1>
+					<h6 className="text-sm" style={{ letterSpacing: "5px" }}>
+						TO XII PPLG 2
+					</h6>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Home
---
+++ src/components/BoxClassIg.jsx
@@ -0,0 +1,18 @@
+const BoxClassIg = () => {
+	return (
+		<div id="BoxClassIg">
+		<a href="https://instagram.com/xii.pplg.2?igshid=OGQ5ZDc2ODk2ZA==">
+				<div className="flex justify-between relative ">
+					<img src="/Instagram.svg" alt="" className="w-auto h-10" />
+					<img src="/next.png" alt="" className="h-4 w-4" />
+				</div>
+				<h1 className="text-white text-md font-semibold pr-3 my-2">
+					Class Instagram
+				</h1>
+				<div className="text-white flex py-2 opacity-60 absolute bottom-1 text-xs">View More</div>
+			</a>
+		</div>
+	)
+}
+
+export default BoxClassIg
